aba660d5d24b910d924059236e0fed83528c0ed5
--local <evandro.slipjr@gmail.com>
2024-10-22 19:26:45 -0300

    feat: render a item with threeJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+
+const path = "/models/cube.glb";
+
 function App() {
-  return <div></div>;
+  const { scene } = useGLTF(path);
+
+  return (
+    <div className="w-full h-[100dvh] bg-gray-500">
+      <Canvas>
+        <primitive object={scene} />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
+
+useGLTF.preload(path);
