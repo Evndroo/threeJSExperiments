b9a2f2fae32936c1e31502b202809ea2106e35a7
--local <evandro.slipjr@gmail.com>
2024-10-22 19:46:29 -0300

    refactor: avoid using extra lib to load glb file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Canvas } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 const path = "/models/cube.glb";
 
 function App() {
-  const { scene } = useGLTF(path);
+  const gltf = useLoader(GLTFLoader, path);
 
   return (
     <div className="w-full h-[100dvh] bg-gray-500">
@@ -12,12 +12,10 @@ function App() {
         <ambientLight intensity={1} />
         <directionalLight position={[10, 10, 10]} intensity={1} />
 
-        <primitive object={scene} rotation={[6.7, -Math.PI / 4, 0]} />
+        <primitive object={gltf.scene} rotation={[6.7, -Math.PI / 4, 0]} />
       </Canvas>
     </div>
   );
 }
 
 export default App;
-
-useGLTF.preload(path);
